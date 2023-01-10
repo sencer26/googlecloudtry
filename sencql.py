a002edf00bc81f023b3f5bee13bdb7cf54f195f2
sencer26 <65547890+sencer26@users.noreply.github.com>
2023-01-10 14:50:00 +0000

    Update sencql.py

--- sencql.py
+++ sencql.py
@@ -1,12 +1,16 @@
 import mysql.connector
-  
+
 dataBase = mysql.connector.connect(
   host ="104.154.109.128",
   user ="root",
-  passwd ="root1234",
-  database = "Persons.newtable"
+  passwd =">SbEPMLp=e_o[Vb#",
+  database = "User_Dashboard"
 )
 
+ 
+
 print(dataBase)
+
+ 
 
 dataBase.close()
