755c91cd8fd1b83ca2e7632871be58e7482d69e0
sencer26 <65547890+sencer26@users.noreply.github.com>
2023-01-11 00:29:42 +0000

    Update and rename sencql.py to sencql2.py

---
+++ sencql2.py
@@ -0,0 +1,54 @@
+import panel as pn
+import sqlalchemy
+
+pn.extension('tabulator') #,notifications=True)
+
+
+button = pn.widgets.Button(name = 'query')
+table =  pn.widgets.Tabulator()
+
+
+def init_db_connection():
+    db_config = {
+        'pool_size': 5,
+        'max_overflow': 2,
+        'pool_timeout': 30,
+        'pool_recycle': 1800,
+    }
+    return init_unix_connection_engine(db_config)
+
+def init_unix_connection_engine(db_config):    
+    pool = sqlalchemy.create_engine(
+        sqlalchemy.engine.url.URL(
+            drivername="postgres+pg8000",
+            query={"unix_sock": "/cloudsql/jupyter-1-09042020:us-central1:sencql/.s.PGSQL.5432"}
+            username="root",
+            password=">SbEPMLp=e_o[Vb#",
+            database="User_Dashboard",
+        ),
+        **db_config
+    )
+    pool.dialect.description_encoding = None
+    return pool
+
+db = init_db_connection()
+
+
+def on_click(event):
+   
+    print('Started')
+
+    with db.connect() as conn:
+        # Execute the query and fetch all results
+        recent_votes = conn.execute(
+            "SELECT candidate, time_cast FROM votes "
+            "ORDER BY time_cast DESC LIMIT 5"
+        ).fetchall()
+
+
+button.on_click(on_click)
+pn.Column(
+    button,
+    table
+).servable();
+#
